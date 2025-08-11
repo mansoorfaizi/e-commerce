cc8bcd24bc5fc64ed6db0f3533b6c4d4c4d7eb6f
Sakina Rasa <sakina.rasa2020@gmail.com>
2025-08-11 13:45:56 +0430

    Create Category model with timestamps

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.product import Product
 from .models.productImage import ProductImage
-
+from .models.category import Category
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(Category)
