d600e9d158e574a881f102d43d7d896f391ff53a
khatira dawari <khatiradawari913@gmail.com>
2025-08-19 09:37:16 -0700

    conversation model,,,

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,4 @@
 from .product import Product
 from .productImage import ProductImage
 from .review import Review  
+from .conversation import Conversation
---
+++ api/models/conversation.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Conversation: {self.title} (User: {self.user.username})"
+    
