458da0c85dd0611fc781113ed0cb7b4d3f23d688
khatira dawari <khatiradawari913@gmail.com>
2025-08-21 05:21:56 -0700

    conversation and supportChat model is updeated

--- api/admin.py
+++ api/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models.product import Product
 from .models.productImage import ProductImage
 from .models.review import Review   
-
+from .models.conversation import Conversation
+from .models.support_chat import SupportChat
 
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Review)
+admin.site.register(Conversation)
+admin.site.register(SupportChat)
