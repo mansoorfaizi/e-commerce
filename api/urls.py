da70a5fb4dca8e4406b88b2f9031297691204d29
Sakina Rasa <sakina.rasa2020@gmail.com>
2025-09-10 17:25:04 +0430

    Add OrderViewSet and OrderItemViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views.category import CategoryViewSet
+from api.views.order import OrderViewset, OrderItemViewset
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'orders',OrderViewset , basename='order')
+router.register(r'order-items',OrderViewset , basename='orderitem')
 
 urlpatterns = [
     path('', include(router.urls)),
