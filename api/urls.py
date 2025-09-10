05e7c427d58eda134b11e8b3522b525d186b172d
Sultan Mohammad <sultanmohammadazizi6@gmail.com>
2025-09-10 18:24:25 +0430

    Implemented Review view using ModelViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views.category import CategoryViewSet
+from api.views.review import ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'reviews', ReviewViewSet, basename='review')
+
 
 urlpatterns = [
     path('', include(router.urls)),
