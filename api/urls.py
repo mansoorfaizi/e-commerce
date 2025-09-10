5da54a7ddf1a15c09ff5e09b7248205b2a6d1cca
Mansoor <mansoorfaizi.cs05@gmail.com>
2025-09-10 10:41:54 +0430

    updated the directory structure

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views.category import CategoryViewSet
 
-
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
 
