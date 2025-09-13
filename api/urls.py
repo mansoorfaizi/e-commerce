daca6b1eb5f4aa4fc74d29873a6aba2aa8b86bac
Khalid Ahmad Muadib <khalidahmadmuadib365@gmail.com>
2025-09-13 10:26:04 -0700

    view has created for payment using ModelViewSet

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views.category import CategoryViewSet
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views.payment import PaymentViewSet  # import from payment.py
+
+router = DefaultRouter()
+router.register(r'payment', PaymentViewSet , basename='payment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
 
